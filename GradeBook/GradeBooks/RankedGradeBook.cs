2050c34914b49056f86c45f1c0afa2a0a613a90a
Dadamsky <damian.adamski@microsoft.wsei.edu.pl>
2023-04-07 21:41:40 +0200

    Return A if the input grade is in the top 20% of the class.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -19,8 +19,14 @@ namespace GradeBook.GradeBooks
         {
             if(Students.Count < 5)
             {
-                throw new InvalidOperationException("You must have at least 5 students to do ranked grading");
+                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
             }
+
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (averageGrade >= grades[threshold - 1])
+                return 'A';
 
             return 'F';
         }
