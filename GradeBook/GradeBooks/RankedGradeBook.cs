64d578fb481ed26f0d46db03db0515dd4553ca11
Dadamsky <damian.adamski@microsoft.wsei.edu.pl>
2023-04-07 21:32:23 +0200

    Override RankedGradeBook's GetLetterGrade method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -8,8 +8,21 @@ namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
-        public RankedGradeBook(string name) : base(name) {
+        public RankedGradeBook(string name) : base(name) 
+        {
+
             Type = Enums.GradeBookType.Standard;
+
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException("You must have at least 5 students to do ranked grading");
+            }
+
+            return 'F';
         }
     }
 }
